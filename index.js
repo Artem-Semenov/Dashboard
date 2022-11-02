ae97fc097783dac0e96b42160357e24074bce908
Artem Semenov <artem.semenov.101@gmail.com>
2022-11-02 14:02:52 +0200

    Mobile swiper

--- index.js
+++ index.js
@@ -362,6 +362,41 @@ class Dashboard {
         dashboard.burgerMenu.classList.remove("burger-menu-active");
       }
     });
+
+    let x1 = null;
+    let y1 = null;
+
+    this.bottomSliderLine.addEventListener("touchstart", function (e) {
+      const firstTouch = e.touches[0];
+      x1 = firstTouch.clientX;
+      y1 = firstTouch.clientY;
+    });
+
+    this.bottomSliderLine.addEventListener("touchmove", function (event) {
+      if (!x1 || !y1) {
+        return false;
+      }
+      let x2 = event.touches[0].clientX;
+      let y2 = event.touches[0].clientY;
+      let xDiff = x2 - x1;
+      let yDiff = y2 - y1;
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+          testRange.value -= 1;
+          dashboard.bottomSliderLine.style.left = testRange.value * -200 + "px";
+          x1 = null;
+          y1 = null;
+        } else {
+          testRange.value++;
+          console.log("right");
+          dashboard.bottomSliderLine.style.left = testRange.value * -200 + "px";
+          x1 = null;
+          y1 = null;
+        }
+      } else {
+        return;
+      }
+    });
   }
 }
 
